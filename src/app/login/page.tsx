781a85cbe7e2200bfe54cf2de778ff5f98212b50
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-08-22 15:44:08 -0300

    ft: login and new account pages created

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import { ProfileForm } from '@/components/Form';
+
+export default function Login() {
+  return (
+    <>
+      <section className="min-h-screen flex justify-center items-center">
+        <ProfileForm />
+      </section>
+    </>
+  );
+}
---
+++ src/app/new-account/page.tsx
@@ -0,0 +1,11 @@
+import { CardToRegisterAdmin } from '@/components/CardRegister';
+
+export default function NewAccount() {
+  return (
+    <>
+      <section className="bg-custom-gradient min-h-screen flex justify-center items-center">
+        <CardToRegisterAdmin />
+      </section>
+    </>
+  );
+}
