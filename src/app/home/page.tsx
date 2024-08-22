cda68a991d4c496f9ab8ac4858569afd603e1e40
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-08-22 15:43:27 -0300

    ft: home page, root page sending to home and layout config to context and toast

---
+++ src/app/home/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { PaginationDemo } from '@/components/Pagination';
+import { InputWithButton } from '@/components/Search';
+import { TableDemo } from '@/components/Table';
+import { Button } from '@/components/ui/button';
+import AddUserIcon from '@/assets/add-user-icon.png';
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <>
+      <section className="min-h-screen flex-col justify-center items-center gap-4 p-12">
+        <div className="flex justify-between items-center mb-10">
+          <InputWithButton />
+          <Button onClick={() => router.push('/client')}>Criar Cliente</Button>
+        </div>
+        <TableDemo />
+        <PaginationDemo />
+      </section>
+    </>
+  );
+}
