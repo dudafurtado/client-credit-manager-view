c8d1680236e38cb6cb2c02e5a05a148652c0e4d6
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-08-23 16:02:38 -0300

    ft: initial client object for use state

---
+++ src/utils/clientObject.ts
@@ -0,0 +1,27 @@
+export const clientObject = {
+  id: 0,
+  name: '',
+  surname: '',
+  email: '',
+  birth_date: '',
+  phone: '',
+  address: {
+    id: 0,
+    zip_code: '',
+    state: '',
+    city: '',
+    street: '',
+    neighborhood: '',
+    additional_information: '',
+    client_id: 0,
+  },
+  cards: [
+    {
+      id: 1,
+      number: '',
+      expire_date: '',
+      CVV: '',
+      client_id: 3,
+    },
+  ],
+};
