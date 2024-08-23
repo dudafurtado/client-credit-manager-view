e87d14d1caebb108b27e257546c2fd8dbbedb7bb
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-08-23 16:03:35 -0300

    ft: funciton to fetch data

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+'use client';
+export default async function fetchData(path: string, method: string, content?: string) {
+  const url = `http://127.0.0.1:8000/api${path}`;
+
+  if (method === 'GET' || method === 'DELETE') {
+    return await fetch(url, { method });
+  }
+
+  return await fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: content ? JSON.stringify(content) : '',
+  });
+}
