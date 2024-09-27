4e91311fa297aab7fad3cf4608fbb5d587fa1001
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-09-27 19:17:53 -0300

    ft: redirect to new account without token at home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,9 +4,19 @@ import { PaginationDemo } from '@/components/Pagination';
 import { InputWithButton } from '@/components/Search';
 import { TableDemo } from '@/components/Table';
 import { Button } from '@/components/ui/button';
+import { useEffect } from 'react';
+import { getToken } from '@/utils/token';
 
 export default function Home() {
   const router = useRouter();
+  const token = getToken();
+
+  useEffect(() => {
+    if (!token) {
+      return router.push('/new-account');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
